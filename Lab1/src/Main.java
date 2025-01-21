e0c4e8b0312d62f36dce458770995eb584198251
IllyaShparuk <illya.shparuk@gmail.com>
2025-01-21 19:52:30 +0200

    IO implementation

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,14 +1,41 @@
+import java.awt.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
-        TextAnalyzer textAnalyzer = new TextAnalyzer("text.txt");
-        System.out.println(textAnalyzer.countWords());
-        System.out.println(textAnalyzer.originalWordsCount());
-        System.out.println(textAnalyzer.countSentences());
-        System.out.println(textAnalyzer.countPunctuations());
-        System.out.println(textAnalyzer.calculateWordsAverage());
-        System.out.println(textAnalyzer.calculateSentencesAverage());
+        Dialog dialog = new Dialog(null, true);
+        FileDialog chooseAFile = new FileDialog(dialog, "Choose a file", FileDialog.LOAD);
+        chooseAFile.setVisible(true);
+        chooseAFile.setFilenameFilter((_, name) -> name.endsWith(".txt"));
+        String filename = chooseAFile.getFile();
+        String directory = chooseAFile.getDirectory();
+        if (filename == null && directory == null)
+            System.out.println("You cancelled the choice");
+        else
+            System.out.println("You chose " + filename);
+        chooseAFile.dispose();
+        dialog.dispose();
 
-        System.out.println("Top 10 words:");
-        textAnalyzer.firstTenPopulatedWordsCount();
+
+        TextAnalyzer textAnalyzer = new TextAnalyzer(directory + filename);
+        try {
+            PrintWriter writer = new PrintWriter("result.txt", StandardCharsets.UTF_8);
+            writer.println("Word Count: " + textAnalyzer.countWords());
+            writer.println("Original Word Count: " + textAnalyzer.originalWordsCount());
+            writer.println("Sentence Count: " + textAnalyzer.countSentences());
+            writer.println("Punctuation Count" + textAnalyzer.countPunctuations());
+            writer.println("Word's Average Length: " + textAnalyzer.calculateWordsAverage());
+            writer.println("Sentence's Average Length: " + textAnalyzer.calculateSentencesAverage());
+            writer.println("\nTop 10 words:");
+            textAnalyzer.firstTenPopulatedWordsCount(writer);
+            writer.close();
+
+            System.out.println("\nResult is written to result file");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- Lab1/src/TextAnalyzer.java
+++ Lab1/src/TextAnalyzer.java
@@ -1,11 +1,11 @@
 import org.apache.commons.io.IOUtils;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class TextAnalyzer {
     private final String text;
@@ -16,7 +16,7 @@ public class TextAnalyzer {
         try {
             FileInputStream fileReader = new FileInputStream(fileName);
             text = IOUtils.toString(fileReader, StandardCharsets.UTF_8);
-            System.out.println(text);
+            System.out.println("\nYour text is: " + text);
             words = text.split("\\W+");
             sentences = text.split("(?<=[.!?])\\s+(?=[A-Z\"'])");
         } catch (IOException e) {
@@ -51,14 +51,19 @@ public class TextAnalyzer {
         return Arrays.stream(sentences).mapToInt(sentence -> sentence.split("\\W+").length).sum() / sentences.length;
     }
 
-    public void firstTenPopulatedWordsCount() {
+    public void firstTenPopulatedWordsCount(@Nullable PrintWriter writer) {
         Map<String, Long> wordFrequency = new HashMap<>();
         for (String word : words) {
             wordFrequency.put(word, wordFrequency.getOrDefault(word, 0L) + 1);
         }
-        wordFrequency.entrySet().stream()
+        var topTenWords = wordFrequency.entrySet().stream()
                 .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-                .limit(10)
-                .forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
+                .limit(10);
+        if (writer == null) {
+            topTenWords.forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
+        } else {
+            topTenWords.forEach(e -> writer.println(e.getKey() + " " + e.getValue()));
+        }
+
     }
 }
