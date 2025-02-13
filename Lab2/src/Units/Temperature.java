e9facab193ad757f906782007d53c1efc7cbc848
IllyaShparuk <illya.shparuk@gmail.com>
2025-02-13 12:13:40 +0200

    int / int problem fix

--- Lab2/src/Units/Temperature.java
+++ Lab2/src/Units/Temperature.java
@@ -2,11 +2,11 @@ package Units;
 
 public class Temperature extends Unit {
     public Temperature() {
-        units.put("From Celsius To Fahrenheit", celsius -> celsius * (9 / 5) + 32);
-        units.put("From Fahrenheit To Celsius", fahrenheit -> (fahrenheit - 32) * (5 / 9));
+        units.put("From Celsius To Fahrenheit", celsius -> celsius * (9.0 / 5.0) + 32.0);
+        units.put("From Fahrenheit To Celsius", fahrenheit -> (fahrenheit - 32.0) * (5.0 / 9.0));
         units.put("From Celsius To Kelvin", celsius -> celsius + 273.15);
         units.put("From Kelvin To Celsius", kelvin -> kelvin - 273.15);
-        units.put("From Fahrenheit To Kelvin", fahrenheit -> (5 / 9) * (fahrenheit + 459.67));
-        units.put("From Kelvin To Fahrenheit", kelvin -> (9 / 5) * kelvin - 459.67);
+        units.put("From Fahrenheit To Kelvin", fahrenheit -> (5.0 / 9.0) * (fahrenheit + 459.67));
+        units.put("From Kelvin To Fahrenheit", kelvin -> (9.0 / 5.0) * kelvin - 459.67);
     }
 }
--- Lab2/src/Units/Time.java
+++ Lab2/src/Units/Time.java
@@ -2,11 +2,11 @@ package Units;
 
 public class Time extends Unit {
     public Time(){
-        units.put("From Seconds To Minutes", seconds -> seconds / 60);
-        units.put("From Minutes To Seconds", minutes -> minutes * 60);
-        units.put("From Minutes To Hours", minutes -> minutes / 60);
-        units.put("From Hours To Minutes", hours -> hours * 60);
-        units.put("From Hours To Seconds", hours -> hours * 3600);
-        units.put("From Seconds To Hours", seconds -> seconds / 3600);
+        units.put("From Seconds To Minutes", seconds -> seconds / 60.0);
+        units.put("From Minutes To Seconds", minutes -> minutes * 60.0);
+        units.put("From Minutes To Hours", minutes -> minutes / 60.0);
+        units.put("From Hours To Minutes", hours -> hours * 60.0);
+        units.put("From Hours To Seconds", hours -> hours * 3600.0);
+        units.put("From Seconds To Hours", seconds -> seconds / 3600.0);
     }
 }
